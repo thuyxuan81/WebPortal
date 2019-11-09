51967e0bf3724020111de58a87b473d0f15288da
Fahad Siddiqi <fahad.a.sidd@gmail.com>
2019-11-08 19:41:40 -0600

    Updated server.js and package.json

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req,res) => res.send('API Running'));
+
+app.use('/api/users', require('./routes/api/users'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

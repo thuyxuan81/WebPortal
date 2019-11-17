b01c5831378d68d708d25396582872d631323cec
Fahad Siddiqi <fasiddiqi4@uh.edu>
2019-11-17 16:44:30 -0600

    Updated server.js file to work with npm

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express();
 
 app.get('/', (req,res) => res.send('API Running'));
 
-app.use('/api/users', require('./routes/api/users'));
-
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

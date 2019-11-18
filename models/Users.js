e03a2062542ffcd4ef020818d9cba903948d19d0
Fahad Siddiqi <fahad.a.sidd@gmail.com>
2019-11-17 19:25:07 -0600

    Created Users Schema for DB with fields

---
+++ models/Users.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const UsersSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model('user', UsersSchema);

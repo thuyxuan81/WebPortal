2a00d7289477226c6a903ef1672b05afe108fbf6
Fahad Siddiqi <fasiddiqi4@uh.edu>
2019-11-17 19:09:48 -0600

    Added code for users.js, auth.js and added connection to DB

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const express = require("express");
+const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+connectDB();
+
 app.get('/', (req,res) => res.send('API Running'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 

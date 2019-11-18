cf60e533d2200c02f1058eb444e6e808620eb6a6
Fahad Siddiqi <fasiddiqi4@uh.edu>
2019-11-17 22:25:12 -0600

    added validators for information to be added in DB

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-
-router.get('/', (req,res) => res.send('User Route'));
+router.post(
+  '/', 
+  [
+    check('firstName', 'First name is required')
+      .not()
+      .isEmpty(),
+    check('lastName', 'Last name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please enter valid email address')
+      .isEmail(),
+    check('password', 'Please enter a password with 8 or more characters')
+      .isLength({ min: 8}),
+    check('role', 'Role is required')
+      .not()
+      .isEmpty()
+  ],
+  (req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({errors: errors.array()})
+    }
+    res.send('User Route');
+});
 
 module.exports = router;

55b2c83f18cfc2103fc805697012d785d70c29f6
Fahad Siddiqi <fasiddiqi4@uh.edu>
2019-12-01 21:07:25 -0600

    Added info for models/Profile.js

---
+++ models/Profile.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  company: {
+    type: String
+  }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);

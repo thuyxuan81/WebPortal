64f9160a3a149afa5eb9b852eea7d52bceefe99a
Thuy Nguyen <thuyxuan.nguyen81@gmail.com>
2019-12-03 16:49:47 -0600

    Add alerts to log in

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,16 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/layout/Login';
+import{Provider} from 'react-redux';
+import store from './store';
+
+
+
+
 import './App.css';
 
 const App = () =>(
+    <Provider store ={store}> 
  <Router>
     <Fragment>
      <Navbar/>
@@ -17,6 +24,7 @@ const App = () =>(
      </section>
     </Fragment>
  </Router>
+ </Provider>
    
 );
 

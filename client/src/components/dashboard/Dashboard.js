94f6438021d1acea271f3d0feea19e70295fc960
Fahad Siddiqi <fahad.a.sidd@gmail.com>
2019-12-04 03:52:39 -0600

    Added buttons for Sales Admin

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -11,7 +11,7 @@ const Dashboard = ({getCurrentProfile, auth: {user}, profile: {profile, loading}
   useEffect(() => {
   getCurrentProfile();} , []);
   
-  if(role == 'Sale Admin') {
+  if(role == 'Sales Admin') {
   return (
     <Fragment>
       <h1 className= 'large text-primary' >Dashboard</h1>
@@ -20,14 +20,39 @@ const Dashboard = ({getCurrentProfile, auth: {user}, profile: {profile, loading}
         is {user && role}
       </p>
       <p>
+        <Link to='/Manage-User-Accounts' className= "btn btn-primary my-1">
+          Manage User Accounts
+        </Link>
+      </p>
+      <p>
+        <Link to='/Assign-Roles' className= "btn btn-primary my-1">
+          Assign Roles 
+        </Link>
+      </p>
+      <p>
+        <Link to='/Help-Desk' className= "btn btn-primary my-1">
+          Help Desk
+        </Link>
+      </p>
+      <p>
         <Link to='/Sales-Reports' className= "btn btn-primary my-1">
           Sale Reports
+        </Link>
+      </p>
+      <p>
+        <Link to='/Sales-Leads' className= "btn btn-primary my-1">
+          Sale Leads
+        </Link>
+      </p>
+      <p>
+        <Link to='/Sales-Demo' className= "btn btn-primary my-1">
+          Sales Demo
         </Link>
       </p>
     </Fragment>
   )
   }
-  if(role == 'Sale Admin') {
+  if(role == 'Finance Admin') {
     return (
       <Fragment>
         <h1 className= 'large text-primary' >Dashboard</h1>
@@ -42,7 +67,55 @@ const Dashboard = ({getCurrentProfile, auth: {user}, profile: {profile, loading}
         </p>
       </Fragment>
     )
-    }
+  }
+  if(role == 'HR Admin') {
+    return (
+      <Fragment>
+        <h1 className= 'large text-primary' >Dashboard</h1>
+        <p className = 'lead'>
+          <i className = 'fas fa-user' /> Welcome {user && firstName} {user && lastName} your role
+          is {user && role}
+        </p>
+        <p>
+          <Link to='/Sales-Reports' className= "btn btn-primary my-1">
+            Other Reports
+          </Link>
+        </p>
+      </Fragment>
+    )
+  }
+  if(role == 'Engineering Admin') {
+    return (
+      <Fragment>
+        <h1 className= 'large text-primary' >Dashboard</h1>
+        <p className = 'lead'>
+          <i className = 'fas fa-user' /> Welcome {user && firstName} {user && lastName} your role
+          is {user && role}
+        </p>
+        <p>
+          <Link to='/Sales-Reports' className= "btn btn-primary my-1">
+            Other Reports
+          </Link>
+        </p>
+      </Fragment>
+    )
+  }
+  if(role == 'Admin') {
+    return (
+      <Fragment>
+        <h1 className= 'large text-primary' >Dashboard</h1>
+        <p className = 'lead'>
+          <i className = 'fas fa-user' /> Welcome {user && firstName} {user && lastName} your role
+          is {user && role}
+        </p>
+        <p>
+          <Link to='/Sales-Reports' className= "btn btn-primary my-1">
+            Other Reports
+          </Link>
+        </p>
+      </Fragment>
+    )
+  }
 };
 
 

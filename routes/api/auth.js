2b71ec463bae95dc34be367e4cb574540e8b227f
Thuy Nguyen <thuyxuan.nguyen81@gmail.com>
2019-12-03 21:42:39 -0600

    Finished log in authentication

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -41,11 +41,9 @@ router.post(
       const {email, password} = req.body;
   
       try {
-  
-      let user = await User.findOne({email});
-  
-      if(!user) {
-       res.status(400).json({errors: [{ msg: 'Invalid Credentials no user' }]});
+         let user = await User.findOne({email});
+         if(!user) {
+           return res.status(400).json({errors: [{ msg: 'Invalid Credentials no user' }]});
       }
 
       const match = await bcrypt.compare(password, user.password);
